d3109174310e72de25e65f16077f6a18669b989d
USER <USER@USER6-PC>
2018-09-21 16:44:11 +0900

    PAGECOUNT 변경

--- Board/src/kr/itedu/board/common/Var.java
+++ Board/src/kr/itedu/board/common/Var.java
@@ -3,5 +3,5 @@ package kr.itedu.board.common;
 public class Var {
 	public static final String[] TITLES = { "공지", "자유", "유머", "정치" };
 	public static final String TEMPLATE_1 = "jsp/template.jsp";
-	public static final int PAGECOUNT = 15;
+	public static final int PAGECOUNT = 10;
 }
